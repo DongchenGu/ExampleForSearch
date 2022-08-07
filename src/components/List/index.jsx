af2f3c39e3249a0503040c76a65b86ae3e808277
nobert <nobertdalton@gmail.com>
2022-08-07 17:04:21 -0400

    update-version3

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import "./index.css"
-
+import PubSub from "pubsub-js";
 
 export default class List extends React.Component{
 
+    //初始化状态，
+    state={
+        users:[],//users初始值为数组
+        isFirst: true, //是否为第一次打开页面
+        isLoading:false, //标识是否处于加载中
+        err: '',  //存储请求相关的错误信息
+    }
+
+    componentDidMount(){
+        PubSub.subscribe('message',(message,data)=>{
+           this.setState(data);
+        });
+    }
+
     render(){
-        const{users,isFirst,isLoading,err} = this.props
+        const{users,isFirst,isLoading,err} = this.state
         return(                   
                 <div className="row">
                 {
--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 import axios from 'axios';
+import PubSub from "pubsub-js";
+
 
 export default class Search extends React.Component{
 
     search = ()=>{
+        
         //获取用户的输入
         const {keywordElement:{value: keyword}}=  this;
         console.log(keyword);
-        //发送请求前通知APP更新状态
-        this.props.updateAppState({isFirst:false,isLoading: true});
+        //发送请求前通知List更新状态\
+        PubSub.publish('message', {isFirst:false,isLoading: true})
         //发送网路请求
         axios.get(`http://api.github.com/search/users?q=${keyword}`).then(
             response => {
-                //请求成功后通知app更新状态
-                this.props.updateAppState({users:response.data.items,isLoading:false});
+                //请求成功后通知List更新状态
+                PubSub.publish('message', {users:response.data.items,isLoading:false})
             },
             error => {
-                //请求失败后通知APP更新状态
-                this.props.updateAppState({err:error.message,isLoading:false}); }
+                //请求失败后通知List更新状态
+                PubSub.publish('message', {err:error.message,isLoading:false})}
+
         )
 
     }
